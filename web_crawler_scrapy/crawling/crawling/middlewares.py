022a5c91199331ee9b0fea9c88b16f657bbbdf31
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-11-27 12:05:55 -0300

    Criando um crawler com output e adicionando exemplo de configuração para proxy server

--- web_crawler_scrapy/crawling/crawling/middlewares.py
+++ web_crawler_scrapy/crawling/crawling/middlewares.py
@@ -78,6 +78,7 @@ class CrawlingDownloaderMiddleware:
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
+        request.meta['proxy'] = '127.0.0.1 '
         return None
 
     def process_response(self, request, response, spider):
